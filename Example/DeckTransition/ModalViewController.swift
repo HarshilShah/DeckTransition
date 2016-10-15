48274dbdc43c5383e4a2cb6fcb5dfe4b509f39ea
harshilshah <harshilshah1910@outlook.com>
2016-10-15 18:50:28 +0530

    Added a working example

---
+++ Example/DeckTransition/ModalViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ModalViewController.swift
+//  DeckTransition
+//
+//  Created by Harshil Shah on 15/10/16.
+//  Copyright © 2016 Harshil Shah. All rights reserved.
+//
+
+import UIKit
+import DeckTransition
+
+class ModalViewController: UIViewController, UITextViewDelegate {
+
+    let textView = UITextView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        modalPresentationCapturesStatusBarAppearance = true
+        
+        view.backgroundColor = .white
+        
+        textView.isEditable = false
+        textView.isSelectable = false
+        textView.showsVerticalScrollIndicator = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.systemFont(ofSize: 40, weight: UIFontWeightHeavy)
+        textView.textAlignment = .center
+        textView.text = "This is the presented modal view controller.\n\nWhen youʼre scrolled to the very top of the view, you can swipe downwards to dismiss it.\n\nThe swipe works in one fluid gesture if youʼre scrolling up as well. Scroll around a bit here to give that a shot."
+        view.addSubview(textView)
+        textView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        textView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        textView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
+        textView.delegate = self
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scrollView.isEqual(textView) else {
+            return
+        }
+        
+        if let delegate = transitioningDelegate as? DeckTransitioningDelegate {
+            if scrollView.contentOffset.y <= 0 {
+                scrollView.bounces = false
+                delegate.isDismissEnabled = true
+            } else {
+                scrollView.bounces = true
+                delegate.isDismissEnabled = false
+            }
+        }
+    }
+    
+}
