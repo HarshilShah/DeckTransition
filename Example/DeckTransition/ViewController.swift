3ea670877216a0cbe8c1008314a7b06fc48ff1e1
Harshil Shah <harshilshah1910@me.com>
2016-10-15 18:50:28 +0530

    Added a working example

--- Example/DeckTransition/ViewController.swift
+++ Example/DeckTransition/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import DeckTransition
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 40, weight: UIFontWeightHeavy)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "This is the presenting view controller.\n\nTap anywhere to show the modal."
+        view.addSubview(label)
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewWasTapped))
+        view.addGestureRecognizer(tap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func viewWasTapped() {
+        let modal = ModalViewController()
+        let transitionDelegate = DeckTransitioningDelegate()
+        modal.transitioningDelegate = transitionDelegate
+        modal.modalPresentationStyle = .custom
+        present(modal, animated: true, completion: nil)
     }
-
 }
 
---
+++ Example/Pods/Target Support Files/DeckTransition/DeckTransition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DeckTransition : NSObject
+@end
+@implementation PodsDummy_DeckTransition
+@end
