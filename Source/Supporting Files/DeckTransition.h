e0cc59efa3b47d68ba22e0b9671629b56338bbd7
harshilshah <harshilshah1910@outlook.com>
2017-08-04 13:37:09 +0530

    Added supporting files and test templates

---
+++ Source/Supporting Files/DeckTransition.h
@@ -0,0 +1,17 @@
+//
+//  DeckTransition.h
+//  DeckTransition
+//
+//  Created by Harshil Shah on 4/8/2017.
+//  Copyright © 2017 Harshil Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeckTransition.
+FOUNDATION_EXPORT double DeckTransitionVersionNumber;
+
+//! Project version string for DeckTransition.
+FOUNDATION_EXPORT const unsigned char DeckTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeckTransition/PublicHeader.h>
---
+++ Tests/DeckTransitionTests.swift
@@ -0,0 +1,36 @@
+//
+//  DeckTransitionTests.swift
+//  DeckTransitionTests
+//
+//  Created by Harshil Shah on 4/8/2017.
+//  Copyright © 2017 Harshil Shah. All rights reserved.
+//
+
+import XCTest
+@testable import DeckTransition
+
+class DeckTransitionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
