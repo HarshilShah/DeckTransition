881ddb088c0b9b3ca49e1de854e5e4da658a2fd3
Harshil Shah <harshilshah1910@outlook.com>
2017-10-22 00:01:31 +0530

    Added a script to build docs using Jazzy

---
+++ build-docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+jazzy \
+    --module DeckTransition \
+    --swift-version 4.0 \
+    --author Harshil Shah \
+    --author_url https://twitter.com/HarshilShah1910 \
+    --copyright "Copyright © 2017 Harshil Shah. Available under the MIT License." \
+    --github_url https://github.com/HarshilShah/DeckTransition \
+    --github-file-prefix https://github.com/HarshilShah/DeckTransition/tree/master \
+    --root-url https://harshilshah.github.io/DeckTransition \
+    --output docs/ \
