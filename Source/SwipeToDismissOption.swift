1b18f2f27429ffd7730a826df5d2b774569bc92c
Cezar Pereira <contact@cezar.co>
2018-03-20 11:29:21 -0700

    Add option to only dismiss the presented view controller after the pan gesture ends
    This preserves all the default behaviour and existing API

---
+++ Source/SwipeToDismissOption.swift
@@ -0,0 +1,15 @@
+//
+//  SwipeToDismissOption.swift
+//  DeckTransition
+//
+//  Created by Cezar on 20/03/18.
+//
+
+import Foundation
+
+@objc public enum SwipeToDismissOption: Int {
+
+    case disabled
+    case onThreshold
+    case onRelease
+}
