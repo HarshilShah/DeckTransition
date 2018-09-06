5e0b87c26c467c57e5cdb3c311c2853302b9eb55
Marcel Bradea <mbradea@gmail.com>
2018-09-06 12:26:01 -0400

    Added segue customization for storyboards

--- Source/DeckSegue.swift
+++ Source/DeckSegue.swift
@@ -13,13 +13,18 @@ import UIKit
 /// To use this, set your segue's class to `DeckSegue`, and its `kind` to
 /// `custom`
 public final class DeckSegue: UIStoryboardSegue {
-    
-    var transition: UIViewControllerTransitioningDelegate?
-    
+
+    /// You can customize the segue transition by providing your own
+    /// DeckTransitioningDelegate in 'prepare(for segue: sender:)'
+    /// i.e.
+    /// (segue as? DeckSegue)?.transitioningDelegate
+    ///     = DeckTransitioningDelegate(isSwipeToDismissEnabled: false)
+    public var transitioningDelegate: DeckTransitioningDelegate?
+
     /// Performs the visual transition for the Deck segue.
     public override func perform() {
-        transition = DeckTransitioningDelegate()
-        destination.transitioningDelegate = transition
+        transitioningDelegate = transitioningDelegate ?? DeckTransitioningDelegate()
+        destination.transitioningDelegate = transitioningDelegate
         destination.modalPresentationStyle = .custom
         source.present(destination, animated: true, completion: nil)
     }
